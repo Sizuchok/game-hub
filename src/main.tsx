12df0349be0ec26cadb42d9fd33b1a99b66764de
sizuchok <sizuchok0@gmail.com>
2023-12-19 00:54:13 +0200

    feat: Implement getInfiniteGames hook via react-query

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,19 @@
 import { ChakraProvider, ColorModeScript } from '@chakra-ui/react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { MainPage } from './routing/pages/main-page.tsx'
 import { theme } from './theme/theme.ts'
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode} />
-      <MainPage />
+      <QueryClientProvider client={queryClient}>
+        <MainPage />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
--- src/routing/pages/main-page.tsx
+++ src/routing/pages/main-page.tsx
@@ -1,8 +1,11 @@
-import { Grid, GridItem, Show } from '@chakra-ui/react'
+import { Button, Grid, GridItem, Show } from '@chakra-ui/react'
+import { useGetInfiniteGames } from '../../common/hooks/get-all-games.hook'
 import NavBar from '../../components/layout/nav-bar'
 import { LAYOUT_AREAS } from '../../theme/layout-areas.const'
 
 export const MainPage = () => {
+  const { data, fetchNextPage } = useGetInfiniteGames()
+
   return (
     <Grid
       gridTemplateAreas={{
@@ -16,7 +19,12 @@ export const MainPage = () => {
       <Show above="lg">
         <GridItem area={LAYOUT_AREAS.aside}>ASide</GridItem>
       </Show>
-      <GridItem area={LAYOUT_AREAS.main}>Main</GridItem>
+      <GridItem area={LAYOUT_AREAS.main}>
+        {data?.pages.map(({ results }) =>
+          results.map(game => <div key={game.id}>{game.name}</div>),
+        )}
+        <Button onClick={() => fetchNextPage}> More</Button>
+      </GridItem>
     </Grid>
   )
 }
