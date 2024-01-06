7d06927f949822f2f3b1419eab6c15bf5d75b450
sizuchok <sizuchok0@gmail.com>
2024-01-06 16:04:32 +0200

    feat: Add hover color to game card heading-link

--- src/game/components/game-card.tsx
+++ src/game/components/game-card.tsx
@@ -1,6 +1,7 @@
-import { Box, CardBody, HStack, Heading, Image } from '@chakra-ui/react'
+import { Box, CardBody, HStack, Heading, Image, useColorModeValue } from '@chakra-ui/react'
 import { Link } from 'react-router-dom'
 import backgroundPlaceholder from '../../assets/game-backgroud-image-placeholder.jpg'
+import { ROUTER_KEYS } from '../../common/const/react-router-keys.const'
 import { Game } from '../../common/types/games.types'
 import { getCroppedImageUrl } from '../../services/utils/get-croppod-image-url.util'
 import CriticScore from './critic-score'
@@ -15,11 +16,12 @@ type Props = {
 const GameCard = ({ game }: Props) => {
   const platforms = game.parent_platforms?.map(({ platform }) => platform)
 
+  const hoverColor = useColorModeValue('blackAlpha.600', 'whiteAlpha.600')
+
   return (
     <StyledGameCard
       _hover={{
         transform: 'scale(1.02)',
-        transition: 'all .3s',
       }}
       transform="scale(1)"
       transition="all .3s"
@@ -35,8 +37,14 @@ const GameCard = ({ game }: Props) => {
             <GamePlatforms platforms={platforms} />
             <CriticScore score={game.metacritic} />
           </HStack>
-          <Heading fontSize="2xl">
-            <Link to={`games/${game.slug}`}>{game.name}</Link>
+          <Heading
+            fontSize="2xl"
+            _hover={{
+              color: hoverColor,
+              transition: 'color .4s',
+            }}
+          >
+            <Link to={`${ROUTER_KEYS.GAMES.GAMES}/${game.slug}`}>{game.name}</Link>
           </Heading>
           <RatingIcon rating={game.rating_top} />
         </Box>
